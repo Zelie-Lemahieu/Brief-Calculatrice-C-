37cf67d636cc9ad4ee23b2682cc459d4141d0e58
Zélie Lemahieu <zelielemahieu@gmail.com>
2024-12-19 16:39:00 +0100

    deuxieme commit

---
+++ CalculatriceEnCSharp/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Initialisation des variables:
+            double premierNombre = 0;
+            double deuxiemeNombre = 0;
+            double resultat = 0;
+            char choixMenu = ' ';
+
+            //Choix de l'opération par l'utilisateur:
+            Console.Write("--- Choisir une opération ---" +
+                "\n + --- multiplication" +
+                "\n - --- soustraction" +
+                "\n * --- multiplication" +
+                "\n / --- division" +
+                "\n ^ --- puissance" +
+                "\n v --- racine" +
+                "\n 0 --- QUITTER" +
+                "\n\n Choix : ");
+            choixMenu = char.Parse(Console.ReadLine());
+
+            //REDIRECTION DANS LES DIFFÉRENTS CALCULS
+            switch (choixMenu)
+            {
+                case '+':
+                    Addition();
+                    Console.WriteLine($"La somme de {premierNombre} et {deuxiemeNombre} vaut {resultat}");
+                    break;
+                case '-':
+                    Soustraction();
+                    Console.WriteLine($"La différence de {premierNombre} et {deuxiemeNombre} vaut {resultat}");
+                    break;
+                case '*':
+                    Multiplication();
+                    Console.WriteLine($"Le produit de {premierNombre} et {deuxiemeNombre} vaut {resultat}");
+                    break;
+                case '/':
+                    Division();
+                    Console.WriteLine($"Le quotient de {premierNombre} par {deuxiemeNombre} vaut {resultat}");
+                    break;
+                case '^':
+                    Puissance();
+                    Console.WriteLine($"La puissance de {premierNombre} par {deuxiemeNombre} vaut {resultat}");
+                    break;
+                case '0':
+                    Environment.Exit(0);
+                    break;
+            };
+
+            //ADDITION
+            void Addition()
+            {
+                //Attributions des valeurs:
+                Console.Write("Entrer un premier terme : ");
+                premierNombre = double.Parse(Console.ReadLine());
+                Console.Write("Entrer un deuxième terme : ");
+                deuxiemeNombre = double.Parse(Console.ReadLine());
+                //Calcul de la somme:
+                resultat = premierNombre + deuxiemeNombre;
+            };
+
+            //SOUSTRACTION:
+            void Soustraction()
+            {
+                //Attributions des valeurs:
+                Console.Write("Entrer un premier terme : ");
+                premierNombre = double.Parse(Console.ReadLine());
+                Console.Write("Entrer un soustracteur : ");
+                deuxiemeNombre = double.Parse(Console.ReadLine());
+                //Calcul de la différence:
+                resultat = premierNombre - deuxiemeNombre;
+            };
+
+            //MULTIPLICATION:
+            void Multiplication()
+            {
+                //Attributions des valeurs:
+                Console.Write("Entrer un premier facteur : ");
+                premierNombre = double.Parse(Console.ReadLine());
+                Console.Write("Entrer un deuxième facteur : ");
+                deuxiemeNombre = double.Parse(Console.ReadLine());
+                //Calcul du produit:
+                resultat = premierNombre * deuxiemeNombre;
+            };
+
+            //DIVISION:
+            void Division()
+            {
+                //Attributions des valeurs:
+                Console.Write("Entrer un dividende : ");
+                premierNombre = double.Parse(Console.ReadLine());
+                do
+                {
+                    Console.Write("Entrer un diviseur : ");
+                    deuxiemeNombre = double.Parse(Console.ReadLine());
+                    if (deuxiemeNombre == 0)
+                    {
+                        Console.WriteLine("Le diviseur ne peut pas valoir 0.");
+                    };
+                } while (deuxiemeNombre == 0);
+                //Calcul du quotient:
+                resultat = premierNombre / deuxiemeNombre;
+            };
+
+            //PUISSANCE:
+            void Puissance()
+            {
+                //Attributions des valeurs:
+                Console.Write("Entrer un facteur : ");
+                premierNombre = double.Parse(Console.ReadLine());
+                Console.Write("Entrer un exposant : ");
+                deuxiemeNombre = double.Parse(Console.ReadLine());
+                //Calcul du produit:
+                resultat = Math.Pow(premierNombre, deuxiemeNombre);
+            };
+        }
+    }
+}
